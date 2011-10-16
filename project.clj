453e38ed1d4ee85064c41dd1ccff40557ba4e1fd
Matt Hoyt <mrhoyt4@yahoo.com>
2011-10-16 04:46:52 -0500

    Updated readme and uploaded it to clojure jars.

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "A library that provides encoding/decoding in bson for clojure using congomongo."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [congomongo "0.1.7"]]
-  :dev-dependencies [[swank-clojure "1.3.1"]])
+  :dev-dependencies [[swank-clojure "1.3.1"]
+                     [lein-clojars "0.6.0"]])
