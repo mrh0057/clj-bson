3d80ff118c78ca82c29168e7f0960b1ec341dc2b
Matt Hoyt <mrhoyt4@yahoo.com>
2011-10-21 17:19:51 -0500

    0.1.0 release

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject clj-bson "0.1.0-SNAPSHOT"
+(defproject clj-bson "0.1.0"
   :description "A library that provides encoding/decoding in bson for clojure using congomongo."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [congomongo "0.1.7"]]
   :dev-dependencies [[swank-clojure "1.3.1"]
-                     [lein-clojars "0.6.0"]])
+                     [lein-clojars "0.6.0"]]
+  :warn-on-reflection true)
